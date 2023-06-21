b83c05c582574bf4fb91d77fa998031f1a8dbcca
João Camargo <jclaudio00_12@hotmail.com>
2023-06-21 11:31:11 -0300

    adjusted timezone

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,8 @@ import Sequelize from "sequelize";
 
 const connection = new Sequelize('guiapress', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'    
+    dialect: 'mysql',
+    timezone: "-03:00"    
 });
 
 export default connection;
