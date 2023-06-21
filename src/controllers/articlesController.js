9e4be6b4afacc078d4df37a87b99a773ae81de8b
João Camargo <jclaudio00_12@hotmail.com>
2023-06-21 12:03:27 -0300

    business rule delete article

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -3,7 +3,11 @@ import Article from "../models/Article.js"
 import slugify from "slugify";
 
 export const listArticles = (request, response) => {
-    response.render("admin/articles/index")
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        response.render("admin/articles/index", {articles:articles})
+    })
 }
 
 export const newArticle = (request, response) => {
@@ -26,3 +30,18 @@ export const saveArticle = (request, response) => {
         response.redirect("/admin/articles")
     })
 }
+
+export const deleteArticle = (request, response) => {
+  let id = request.body.id;
+  if (id != undefined && !isNaN(id)) {
+    Article.destroy({
+      where: {
+        id: id,
+      }
+    }).then(() => {
+      response.redirect("/admin/articles");
+    });
+  } else {
+    response.redirect("/admin/articles");
+  }
+};
