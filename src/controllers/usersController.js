ceb37273f657a1259aa8cdb8c832359b70eb173d
João Camargo <jclaudio00_12@hotmail.com>
2023-06-22 10:08:02 -0300

    adjusted user email exists and listuser

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,7 +2,9 @@ import User  from "../models/User.js";
 import bcrypt from "bcryptjs"
 
 export const listUsers = (request, response) => {
-    response.send("Lista de usuários")
+    User.findAll().then(users => {
+        response.render("admin/users/index", {users:users})
+    });
 }
 
 export const createUser = (request, response) => {
@@ -13,16 +15,29 @@ export const saveUser = (request, response) => {
     let email = request.body.email;
     let password = request.body.password;
 
-    let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(password, salt);
+    User.findOne({where: {email:email}}).then(user => {
+        if(user == undefined){
 
-    User.create({
-        email:email,
-        password:hash
-    }).then(() => {
-        response.redirect("/");
-    }).catch((error) => {
-        response.redirect("/");
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+               email: email,
+               password: hash,
+            })
+            .then(() => {
+                response.redirect("/admin/users");
+            })
+            .catch((error) => {
+                response.redirect("/");
+            });
+
+
+        }else{
+            response.redirect("/admin/users/create")
+        }
     })
+
+   
 
 };
